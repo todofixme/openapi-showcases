998b7992d411e1de2bdd8a8c95ede1ed6576b867
Johannes Schmidt <github@dofix.me>
2025-02-19 14:33:48 +0100

    add API info

--- src/main/kotlin/me/dofix/springdoc/SpringdocApplication.kt
+++ src/main/kotlin/me/dofix/springdoc/SpringdocApplication.kt
@@ -1,8 +1,16 @@
 package me.dofix.springdoc
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+    info = io.swagger.v3.oas.annotations.info.Info(
+        title = "Shelf Registry API",
+        version = "0.1.0",
+        description = "An REST API to manage authors and books."
+    )
+)
 @SpringBootApplication
 class SpringdocApplication
 
